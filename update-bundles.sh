b37c2a0bea9c3fafe549e1ef8f14137911cbdedc
Hector Rivas Gandara <keymon@gmail.com>
2017-01-13 17:18:37 +0000

    update bundles

--- update-bundles.sh
+++ update-bundles.sh
@@ -8,7 +8,7 @@ set -e
 				cd "$submodule"
 				echo "===> $submodule"
 
-				if git local-branches | grep -q develop; then
+				if git branches | grep -q develop; then
 					git checkout --quiet develop
 				else
 					git checkout --quiet master
